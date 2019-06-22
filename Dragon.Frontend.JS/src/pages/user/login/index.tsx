69bf62cf3b6b9a811d10fcb3f04a3027e770625e
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2019-06-22 23:35:01 +0200

    JS changes

---
+++ Dragon.Frontend.JS/src/pages/user/login/index.tsx
@@ -0,0 +1,150 @@
+import { Alert, Checkbox, Icon } from 'antd';
+import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale';
+import React, { Component } from 'react';
+
+import { CheckboxChangeEvent } from 'antd/es/checkbox';
+import { Dispatch } from 'redux';
+import { FormComponentProps } from 'antd/es/form';
+import Link from 'umi/link';
+import { connect } from 'dva';
+import { StateType } from './model';
+import LoginComponents from './components/Login';
+import styles from './style.less';
+
+const { Tab, UserName, Password, Mobile, Captcha, Submit } = LoginComponents;
+
+interface LoginProps {
+  dispatch: Dispatch<any>;
+  userLogin: StateType;
+  submitting: boolean;
+}
+interface LoginState {
+  type: string;
+  autoLogin: boolean;
+}
+export interface FromDataType {
+  name: string;
+  password: string;
+}
+
+@connect(
+  ({
+    userLogin,
+    loading,
+  }: {
+    userLogin: StateType;
+    loading: {
+      effects: {
+        [key: string]: string;
+      };
+    };
+  }) => ({
+    userLogin,
+    submitting: loading.effects['userLogin/login'],
+  }),
+)
+class Login extends Component<LoginProps, LoginState> {
+  loginForm: FormComponentProps['form'] | undefined | null = undefined;
+
+  state: LoginState = {
+    type: 'account',
+    autoLogin: true,
+  };
+
+  changeAutoLogin = (e: CheckboxChangeEvent) => {
+    this.setState({
+      autoLogin: e.target.checked,
+    });
+  };
+
+  handleSubmit = (err: any, values: FromDataType) => {
+    const { type } = this.state;
+    if (!err) {
+      const { dispatch } = this.props;
+      dispatch({
+        type: 'userLogin/login',
+        payload: {
+          ...values,
+          type,
+        },
+      });
+    }
+  };
+
+  /*onTabChange = (type: string) => {
+    this.setState({ type });
+  };
+
+  onGetCaptcha = () =>
+    new Promise((resolve, reject) => {
+      if (!this.loginForm) {
+        return;
+      }
+      this.loginForm.validateFields(['mobile'], {}, (err: any, values: FromDataType) => {
+        if (err) {
+          reject(err);
+        } else {
+          const { dispatch } = this.props;
+          ((dispatch({
+            type: 'userLogin/getCaptcha',
+            payload: values.mobile,
+          }) as unknown) as Promise<any>)
+            .then(resolve)
+            .catch(reject);
+        }
+      });
+    });*/
+
+  renderMessage = (content: string) => (
+    <Alert style={{ marginBottom: 24 }} message={content} type="error" showIcon />
+  );
+
+  render() {
+    const { userLogin, submitting } = this.props;
+    const { type, autoLogin } = this.state;
+    return (
+      <div className={styles.main}>
+        <LoginComponents
+          defaultActiveKey={type}
+          //onTabChange={this.onTabChange}
+          onSubmit={this.handleSubmit}
+          ref={(form: any) => {
+            this.loginForm = form;
+          }}
+        >
+          <UserName
+            name="userName"
+            placeholder={`${formatMessage({ id: 'user-login.login.userName' })}: admin or user`}
+            rules={[
+              {
+                required: true,
+                message: formatMessage({ id: 'user-login.userName.required' }),
+              },
+            ]}
+          />
+          <Password
+            name="password"
+            placeholder={`${formatMessage({ id: 'user-login.login.password' })}: ant.design`}
+            rules={[
+              {
+                required: true,
+                message: formatMessage({ id: 'user-login.password.required' }),
+              },
+            ]}
+            onPressEnter={() => this.loginForm && this.loginForm.validateFields(this.handleSubmit)}
+          />
+          <div>
+            <Checkbox checked={autoLogin} onChange={this.changeAutoLogin}>
+              <FormattedMessage id="user-login.login.remember-me" />
+            </Checkbox>
+          </div>
+          <Submit loading={submitting}>
+            <FormattedMessage id="user-login.login.login" />
+          </Submit>
+        </LoginComponents>
+      </div>
+    );
+  }
+}
+
+export default Login;
