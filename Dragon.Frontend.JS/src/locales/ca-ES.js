84e895338fd677023d804d748671c7b3bbd1d065
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2019-06-28 22:45:31 +0200

    code removed

--- Dragon.Frontend.JS/src/locales/ca-ES.js
+++ Dragon.Frontend.JS/src/locales/ca-ES.js
@@ -1,5 +1,4 @@
 import component from './ca-ES/component';
-import globalHeader from './ca-ES/globalHeader';
 import menu from './ca-ES/menu';
 import pwa from './ca-ES/pwa';
 import settingDrawer from './ca-ES/settingDrawer';
@@ -11,7 +10,6 @@ export default {
   'layout.user.link.help': 'Ajuda',
   'layout.user.link.privacy': 'Privacitat',
   'layout.user.link.terms': 'Termes d\'ús',
-  ...globalHeader,
   ...menu,
   ...settingDrawer,
   ...settings,
--- Dragon.Frontend.JS/src/locales/en-US.js
+++ Dragon.Frontend.JS/src/locales/en-US.js
@@ -1,5 +1,4 @@
 import component from './en-US/component';
-import globalHeader from './en-US/globalHeader';
 import menu from './en-US/menu';
 import pwa from './en-US/pwa';
 import settingDrawer from './en-US/settingDrawer';
@@ -12,7 +11,6 @@ export default {
   'layout.user.link.privacy': 'Privacy',
   'layout.user.link.terms': 'Terms',
   'app.preview.down.block': 'Download this page to your local project',
-  ...globalHeader,
   ...menu,
   ...settingDrawer,
   ...settings,
