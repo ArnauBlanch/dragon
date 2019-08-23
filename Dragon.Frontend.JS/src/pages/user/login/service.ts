4c6d1b9288c856b07749ce6bcfc724ecad97a99a
Arnau Blanch Cortès <ArnauBlanch@users.noreply.github.com>
2019-08-23 18:46:14 +0200

    Remove Ant Design Pro code

--- Dragon.Frontend.JS/src/pages/user/login/service.ts
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request';
-import { FromDataType } from './index';
-
-export async function checkApiKey(params: FromDataType) {
-  return request('https://caupinyaderosa.azure-api.net/dragon/checkApiKey', {
-    method: 'GET',
-    headers: { 'X-Functions-Key': params.password },
-  });
-}
