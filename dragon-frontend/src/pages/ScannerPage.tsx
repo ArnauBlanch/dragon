a4b8ef9be807532ca5be1529039471a0d61f12e9
Arnau Blanch Cortès <ArnauBlanch@users.noreply.github.com>
2020-03-08 23:32:31 +0100

    switch camera feature

--- dragon-frontend/src/pages/ScannerPage.tsx
+++ dragon-frontend/src/pages/ScannerPage.tsx
@@ -1,5 +1,51 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-console */
+/* eslint-disable react/no-unused-state */
 import React from 'react';
+import { Button } from 'antd';
+import { getDeviceId, changeDevice } from '../helpers/barcode';
+import BarcodeScanner from '../components/BarcodeScanner';
+import SwitchCameraIcon from '../components/SwitchCameraIcon';
+import '../styles/scanner.css';
 
-const ScannerPage: React.FC = () => <div style={{ backgroundColor: 'grey', height: '100vh' }} />;
+type State = { currentDevice?: string; isbn?: string };
+class ScannerPage extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {};
+
+    this.changeDevice = this.changeDevice.bind(this);
+  }
+
+  componentDidMount() {
+    getDeviceId().then((id) => this.setState({ currentDevice: id }));
+  }
+
+  changeDevice() {
+    changeDevice().then((newId) => this.setState({ currentDevice: newId }));
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.currentDevice && (
+          <BarcodeScanner
+            deviceId={this.state.currentDevice}
+            onDetected={console.log}
+            overlay={
+              <Button
+                className="camera-button"
+                shape="circle"
+                size="large"
+                onClick={this.changeDevice}
+                icon={<SwitchCameraIcon />}
+              />
+            }
+          />
+        )}
+      </>
+    );
+  }
+}
 
 export default ScannerPage;
